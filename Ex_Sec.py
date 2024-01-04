d5100a4b0f08f5c415a180a1e5e3b1ed6d6b4de1
Voitenko Artem <bobinrobin12@gmail.com>
2024-01-04 19:54:38 +0300

    trouble in my Ex...

--- Ex_Sec.py
+++ Ex_Sec.py
@@ -1,6 +1,26 @@
-n = int(input('Введите число: '))
+# n = int(input('Введите число: '))
+# result = 1
+# while n > 1:
+#     result *= n
+#     n -= 1
+# print(result)
+
+# def fib(n):
+#     a, b = 0, 1
+#     for __ in range(n):
+#         yield a
+#         a, b = b, a + b
+# return (list(fib(10)))
+
+
+
+n = int(input("Введите число: "))
+a, b = 0, 1
 result = 1
-while n > 1:
-    result *= n
-    n -= 1
-print(result)
+while b <= n:
+    a, b = b, a + b
+    result += 1
+if b == n:
+    print(result)
+else:
+    print(-1)
