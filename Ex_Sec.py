da5fdb1f05877c06674ba6dfab13c8fe65124f77
Voitenko Artem <bobinrobin12@gmail.com>
2024-01-08 11:46:11 +0300

    complete second task

--- Ex_Sec.py
+++ Ex_Sec.py
@@ -5,22 +5,13 @@
 #     n -= 1
 # print(result)
 
-# def fib(n):
-#     a, b = 0, 1
-#     for __ in range(n):
-#         yield a
-#         a, b = b, a + b
-# return (list(fib(10)))
 
-
-
-n = int(input("Введите число: "))
-a, b = 0, 1
-result = 1
-while b <= n:
-    a, b = b, a + b
-    result += 1
-if b == n:
-    print(result)
-else:
-    print(-1)
+# n = int(input("Введите число: "))
+# my_fib = [0, 1]
+# while n > my_fib[-2]:
+#     if my_fib[-1] == n:
+#         print(len(my_fib))
+#     my_fib.append(my_fib[-1] + my_fib[-2])
+# if my_fib[-2] != n:
+#     print(-1)
+    
