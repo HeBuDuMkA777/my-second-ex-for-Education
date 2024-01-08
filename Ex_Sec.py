ddaa5105683e21d1fad608b4d2bda91711dce9cc
Voitenko Artem <bobinrobin12@gmail.com>
2024-01-08 14:59:50 +0300

    complete Task

--- Ex_Sec.py
+++ Ex_Sec.py
@@ -36,3 +36,30 @@
 # max_weight = max(weight_watermelons_list)
 # min_weight = min(weight_watermelons_list)
 # print(f'Самый легкий арбуз для тёщи весит {min_weight} кг., а самый тяжелый, для себя, {max_weight} кг.')
+
+# nums = [1, 1, 2, 0, -1, 3, 4, 4]
+# result = len(set(nums))
+# print(result)
+
+# nums = [1, 2, 3, 4, 5] 
+# k = int(input("Введите ширину сдвига: "))
+# div = k % len(nums)
+# result = nums[div:] + nums[:div]
+# print(*result)
+
+
+# my_list = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}] 
+# result = set()
+# for a in my_list:
+#     value = list(a.values())
+#     result.add(value[0])
+# print(result)
+
+# import random
+# n = int(input("Введите длинну списка: "))
+# my_list = [random.randint(1, 101) for _ in range(n)]
+# counter = 0
+# for i in range(1, len(my_list)):
+#     if my_list[i] > my_list[i-1]:
+#         counter += 1
+# print(counter)
